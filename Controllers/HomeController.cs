d6f33133adcb789afa313a82a4fee1a547f087f2
Khanyisa.Dlodlo <Khanyisa.Dlodlo@KDLODLO-NB>
2023-03-24 09:39:51 +0200

    uncommented method

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,12 +23,12 @@ namespace EmployeeManagement.Controllers
             return View();
         }
 
-        //public ActionResult Contact()
-        //{
-        //    //Returning None existant View 
-        //    // Redundant Method 
-        //    return View();
-        //}
+        public ActionResult Contact()
+        {
+            //Returning None existant View 
+            // Redundant Method 
+            return View();
+        }
 
         //commented out cause of redundancy
 
